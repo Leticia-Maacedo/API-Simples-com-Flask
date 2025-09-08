8d84fac0de53f91da3746ae4744b80b085f6b65b
Leticia Macedo <leticiamacedo203@gmail.com>
2025-09-07 23:42:43 -0300

    Add files via upload

---
+++ MVC-flask/app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from config import Config
+from controllers.user_controller import UserController
+from controllers.task_controller import TaskController
+from models import db  # importa o db do __init__.py
+
+app = Flask(__name__, template_folder=os.path.join('view', 'templates'))
+app.config.from_object(Config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# cria tabelas
+with app.app_context():
+    db.create_all()
+
+# Rotas Usuários
+app.add_url_rule('/', view_func=UserController.index, endpoint='index')
+app.add_url_rule('/contact', view_func=UserController.contact, methods=['GET', 'POST'], endpoint='contact')
+
+# Rotas Tarefas
+app.add_url_rule("/tasks", view_func=TaskController.list_tasks, endpoint="list_tasks")
+app.add_url_rule("/tasks/new", view_func=TaskController.create_task, methods=["GET", "POST"], endpoint="create_task")
+app.add_url_rule("/tasks/update/<int:task_id>", view_func=TaskController.update_task_status, methods=["POST"], endpoint="update_task_status")
+app.add_url_rule("/tasks/delete/<int:task_id>", view_func=TaskController.delete_task, methods=["POST"], endpoint="delete_task")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
